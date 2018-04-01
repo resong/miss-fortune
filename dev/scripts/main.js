034b02311f507c1adea7d85e77e0f0132a1de8f9
Rebecca Song <rebecca.song@outlook.com>
2018-04-01 19:10:37 -0400

    Refactor button name, restructure main.js

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,23 +1,46 @@
-// JS
-let missFortune = {};
+(function() {
 
-missFortune.MAX_QUOTES = 50;
+    'use strict';
 
-missFortune.randoNum;
-missFortune.quoteObject = {
-    quote: '',
-    details: '',
-    source: ''
-};
+    // Set up variables 
+    const MAX_QUOTES = 4;
+    const quoteObject = {
+        quote: '',
+        details: '',
+        source: ''
+    };
+    let randoNum = 10;                  // default index will be 10
+    // let database = firebase.database(); // reference to database service
 
-// Randomize a number onClick
-$('#fortuneButton').on('click', (e) => {
+    // Randomize a number onClick 
+    $('#fortuneBtn').on('click', (e) => {
+        randoNum = Math.floor(Math.random() * MAX_QUOTES);
+        console.log('Random Num: ' + randoNum);
+    });
 
-    missFortune.randoNum = Math.floor(Math.random() * missFortune.MAX_QUOTES);
+})();
 
-    console.log('Random Num: ' + missFortune.randoNum);
 
-});
+// // JS
+// let missFortune = {};
+
+// missFortune.MAX_QUOTES = 50;
+
+// missFortune.randoNum;
+// missFortune.quoteObject = {
+//     quote: '',
+//     details: '',
+//     source: ''
+// };
+
+// // Randomize a number onClick
+// $('#fortuneButton').on('click', (e) => {
+
+//     missFortune.randoNum = Math.floor(Math.random() * missFortune.MAX_QUOTES);
+
+//     console.log('Random Num: ' + missFortune.randoNum);
+
+// });
 
 // Pull object from db based on random number
 
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,24 +1,47 @@
 'use strict';
 
-// JS
-var missFortune = {};
+(function () {
 
-missFortune.MAX_QUOTES = 50;
+    'use strict';
 
-missFortune.randoNum;
-missFortune.quoteObject = {
-    quote: '',
-    details: '',
-    source: ''
-};
+    // Set up variables 
 
-// Randomize a number onClick
-$('#fortuneButton').on('click', function (e) {
+    var MAX_QUOTES = 4;
+    var quoteObject = {
+        quote: '',
+        details: '',
+        source: ''
+    };
+    var randoNum = 10; // default index will be 10
+    // let database = firebase.database(); // reference to database service
 
-    missFortune.randoNum = Math.floor(Math.random() * missFortune.MAX_QUOTES);
+    // Randomize a number onClick 
+    $('#fortuneBtn').on('click', function (e) {
+        randoNum = Math.floor(Math.random() * MAX_QUOTES);
+        console.log('Random Num: ' + randoNum);
+    });
+})();
 
-    console.log('Random Num: ' + missFortune.randoNum);
-});
+// // JS
+// let missFortune = {};
+
+// missFortune.MAX_QUOTES = 50;
+
+// missFortune.randoNum;
+// missFortune.quoteObject = {
+//     quote: '',
+//     details: '',
+//     source: ''
+// };
+
+// // Randomize a number onClick
+// $('#fortuneButton').on('click', (e) => {
+
+//     missFortune.randoNum = Math.floor(Math.random() * missFortune.MAX_QUOTES);
+
+//     console.log('Random Num: ' + missFortune.randoNum);
+
+// });
 
 // Pull object from db based on random number
 
